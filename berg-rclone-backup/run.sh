72eeea4367a56fcf69bd72ec91fd11dbfb818b9f
Bryan Berg <bdb@north-eastham.org>
2021-11-14 19:58:26 -0800

    finish updating config

--- berg-rclone-backup/run.sh
+++ berg-rclone-backup/run.sh
@@ -7,18 +7,18 @@ CONFIG_FILE="/data/options.json"
 bashio::log.info "Backup beginning at $(date)"
 
 bashio::config.require 'rclone'
-bashio::config.require 'rclone.config'
+bashio::config.require 'rclone_config'
 
 bashio::log.info "Writing config"
 
 # can't use bashio::config here because it eats newlines
-jq -r '.rclone.config' "${CONFIG_FILE}" > /tmp/rclone.conf
+jq -r '.rclone_config' "${CONFIG_FILE}" > /tmp/rclone.conf
 
-bashio::config.require 'rclone.remotes'
+bashio::config.require 'rclone_remotes'
 
-remote_indexes=$(jq -r '.rclone.remotes | keys[]' "${CONFIG_FILE}")
+remote_indexes=$(jq -r '.rclone_remotes | keys[]' "${CONFIG_FILE}")
 for index in ${remote_indexes}; do
-    config_base="rclone.remotes[${index}]"
+    config_base="rclone_remotes[${index}]"
     bashio::config.require "${config_base}.definition"
     definition=$(bashio::config "${config_base}.definition")
     extra_args=""
@@ -33,8 +33,8 @@ for index in ${remote_indexes}; do
     bashio::log.info "Finished backup to remote ${definition}"
 done
 
-if bashio::config.has_value 'purge.days'; then
-    days=$(bashio::config 'purge.days')
+if bashio::config.has_value 'purge_days'; then
+    days=$(bashio::config 'purge_days')
     bashio::log.info "Purging backups older than ${days} from disk..."
 
     find /backup -type f -mtime +${days} -exec rm {} \;
